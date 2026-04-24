2ea4e5d116e5c93c8b4a1c08c4536080cd19306b
tztz15243-create <tztz15243@gmail.com>
2026-04-24 12:39:51 +0100

    Fix formatting and add missing newline at end of file

--- autoup.py
+++ autoup.py
@@ -6,6 +6,7 @@ def AuToUpDaTE():
     version = result['version']
     r = requests.get(
         f'https://bdversion.ggbluefox.com/live/ver.php?version={version}&lang=ar&device=android&channel=android&appstore=googleplay&region=ME&whitelist_version=1.3.0&whitelist_sp_version=1.0.0&device_name=google%20G011A&device_CPU=ARMv7%20VFPv3%20NEON%20VMH&device_GPU=Adreno%20(TM)%20640&device_mem=1993'
+        verify=False
     ).json()
     url = r['server_url']
     ob = r['latest_release_version']
