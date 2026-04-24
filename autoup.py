55621352993851ff62b6efb32c280ce768118d12
tztz15243-create <tztz15243@gmail.com>
2026-04-24 13:12:16 +0100

    Add verify=False parameter in requests.get
    Added a comment to clarify the addition of the verify=False parameter.

--- autoup.py
+++ autoup.py
@@ -1,3 +1,4 @@
+# الملف: autoup.py
 import requests
 from google_play_scraper import app
 
@@ -5,8 +6,8 @@ def AuToUpDaTE():
     result = app('com.dts.freefireth', lang="fr", country='fr')
     version = result['version']
     r = requests.get(
-        f'https://bdversion.ggbluefox.com/live/ver.php?version={version}&lang=ar&device=android&channel=android&appstore=googleplay&region=ME&whitelist_version=1.3.0&whitelist_sp_version=1.0.0&device_name=google%20G011A&device_CPU=ARMv7%20VFPv3%20NEON%20VMH&device_GPU=Adreno%20(TM)%20640&device_mem=1993'
-        verify=False
+        f'https://bdversion.ggbluefox.com/live/ver.php?version={version}&lang=ar&device=android&channel=android&appstore=googleplay&region=ME&whitelist_version=1.3.0&whitelist_sp_version=1.0.0&device_name=google%20G011A&device_CPU=ARMv7%20VFPv3%20NEON%20VMH&device_GPU=Adreno%20(TM)%20640&device_mem=1993',
+        verify=False   # <-- أضف هذا السطر فقط
     ).json()
     url = r['server_url']
     ob = r['latest_release_version']
